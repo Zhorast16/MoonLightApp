0e14b82a7dde91fec42e0212cbcfa1e4f6664d31
Zhorast16 <ramadika1692@gmail.com>
2017-12-02 18:05:39 +0700

    update v 1.0.4

--- MoonlightApp/app/src/main/java/com/example/android/moonlightapp/activity/OrderActivity.java
+++ MoonlightApp/app/src/main/java/com/example/android/moonlightapp/activity/OrderActivity.java
@@ -1,23 +1,43 @@
 package com.example.android.moonlightapp.activity;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
-
+import android.content.Intent;
 import com.example.android.moonlightapp.R;
 
 public class OrderActivity extends AppCompatActivity {
-    private ViewPager pager;
-    private TabLayout tabs;
+    int harganasgor, hargamiegor, hargamiereb, hargagtea, hargachoco, hargamoon;
+    int jml, total;
+    CheckBox nasgor, miegor, miereb, gtea, choco, moon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        EditText harganasgor = findViewById(R.id.qty1);
+        EditText hargamiegor = findViewById(R.id.qty2);
+        EditText hargamiereb = findViewById(R.id.qty3);
+        EditText hargagtea = findViewById(R.id.qty4);
+        EditText hargachoco = findViewById(R.id.qty5);
+        EditText hargamoon = findViewById(R.id.qty6);
+        Button orders = (Button) findViewById(R.id.btnOrder);
+        nasgor = findViewById(R.id.nasgor);
+        miegor = findViewById(R.id.miegor);
+        miereb = findViewById(R.id.miereb);
+        gtea = findViewById(R.id.gtea);
+        choco = findViewById(R.id.choco);
+        moon = findViewById(R.id.moon);
+
     }
     @Override
     public boolean onOptionsItemSelected (MenuItem menuItem) {
@@ -29,4 +49,20 @@ public class OrderActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(menuItem);
     }
 
+    private int Total(int order){
+        if (nasgor.isChecked())
+            jml+=harganasgor*10000;
+        if (miegor.isChecked())
+            jml+=hargamiegor*9000;
+        if (miegor.isChecked())
+            jml+=hargamiereb*8000;
+        if (gtea.isChecked())
+            jml+=hargagtea*5000;
+        if (choco.isChecked())
+            jml+=hargachoco*15000;
+        if (moon.isChecked())
+            jml+=hargamoon*25000;
+        return total = jml;
+    }
+    orders.
 }
---
+++ MoonlightApp/app/src/main/java/com/example/android/moonlightapp/adapter/OrderAdapter.java
@@ -0,0 +1,9 @@
+package com.example.android.moonlightapp.adapter;
+
+/**
+ * Created by Ariyan on 12/2/2017.
+ */
+
+public class OrderAdapter {
+
+}
